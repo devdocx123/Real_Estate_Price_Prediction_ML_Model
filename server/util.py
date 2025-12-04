1d2021a9dbb90aa1316c9f693e3d93b8feb8c61c
Saeed Asif <devdocx123@gmail.com>
2025-12-04 21:54:00 +0500

    Update util.py

--- server/util.py
+++ server/util.py
@@ -1,10 +1,10 @@
 import json
 import numpy as np
 import pickle
-import warnings
 import os
+import warnings
 
-warnings.filterwarnings(action='ignore', category=UserWarning)
+warnings.filterwarnings("ignore")
 
 __location = []
 __data_columns = []
@@ -15,59 +15,38 @@ def get_estimated_price(location, sqft, bath, bhk):
         loc_index = __data_columns.index(location.lower())
     except ValueError:
         loc_index = -1
-
     x = np.zeros(len(__data_columns))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
     if loc_index >= 0:
         x[loc_index] = 1
-
     return round(__model.predict([x])[0], 2)
-
 
 def get_location_names():
     return __location
 
-
 def load_saved_artifacts():
-    print("Loading saved artifacts... start")
-
-    global __location
-    global __data_columns
-    global __model
-
+    global __data_columns, __location, __model
     base_dir = os.path.dirname(os.path.abspath(__file__))
-
     columns_path = os.path.join(base_dir, "artifacts", "columns.json")
     model_path = os.path.join(base_dir, "artifacts", "bangalore_home_price_prediction_model.pickle")
 
-    print("columns_path:", columns_path)
-    print("exists?", os.path.exists(columns_path))
-
-    # Load columns
     if os.path.exists(columns_path):
-        with open(columns_path, "r") as f:
+        with open(columns_path) as f:
             data = json.load(f)
-            __data_columns = data.get('data_columns', [])
+            __data_columns = data.get("data_columns", [])
             __location = [loc.lower() for loc in __data_columns[3:]]
-        print("Locations loaded:", __location)
+        print("Locations:", __location)
     else:
-        print("ERROR: columns.json not found!")
-        __data_columns = []
-        __location = []
+        print("ERROR: columns.json missing!")
 
-    # Load model
     if os.path.exists(model_path):
         with open(model_path, "rb") as f:
             __model = pickle.load(f)
-        print("Model loaded successfully")
+        print("Model loaded")
     else:
-        print("ERROR: model file not found!")
-
-    print("loading saved artifacts... done")
-
+        print("ERROR: model pickle missing!")
 
 if __name__ == "__main__":
     load_saved_artifacts()
-    print("Available locations:", get_location_names())
