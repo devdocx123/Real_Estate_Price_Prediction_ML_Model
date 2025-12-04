07fafcffa2dc5aec60a60c968671c656b9e12795
Saeed Asif <devdocx123@gmail.com>
2025-12-04 11:40:12 +0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,4 +1,3 @@
-
 function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
   for(var i in uiBathrooms) {
@@ -27,8 +26,8 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  // var url = "http://127.0.0.1:5000/predict_home_price";  // use this if you are not sing niginx 
-   var url = '/api/predict_home_price'; // use this if you are using nginx 
+  // Updated URL to match Flask backend routes
+  var url = '/predict_home_price'; 
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -44,8 +43,8 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log( "document loaded" );
-   //var url = "http://127.0.0.1:5000/get_location_names"; // use this when not using nginx
-     var url = "/api/get_location_names";
+  // Updated URL to match Flask backend routes
+  var url = "/get_location_names";
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
