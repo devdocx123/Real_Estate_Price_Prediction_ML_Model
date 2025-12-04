3da6099be217b169e7dfe6198344e3d4fa8ffccd
Saeed Asif <devdocx123@gmail.com>
2025-12-04 11:39:40 +0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,38 +1,43 @@
-from flask import Flask , request , jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import util
+import os
+
 app = Flask(__name__)
 
-
+# ---------------- API Routes ----------------
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
-        'locations' : util.get_location_names()
+        'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
-@app.route('/predict_home_price' , methods=['POST'])
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price' : util.get_estimated_price(location, total_sqft, bath, bhk)
-
+        'estimated_price': util.get_estimated_price(location, total_sqft, bath, bhk)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
+# ---------------- Serve Frontend ----------------
+CLIENT_DIR = os.path.join(os.path.dirname(__file__), '../client')
 
+@app.route('/')
+def index():
+    return send_from_directory(CLIENT_DIR, 'index.html')
 
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(CLIENT_DIR, path)
 
-import os
-
+# ---------------- Main ----------------
 if __name__ == '__main__':
     util.load_saved_artifacts()
     port = int(os.environ.get('PORT', 10000))
